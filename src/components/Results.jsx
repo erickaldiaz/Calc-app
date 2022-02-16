29cd4b987570be17aa003ceca57a204f3cec6279
Ericka Diaz <erickaldiaz@gmail.com>
2022-02-16 14:13:23 -0300

    edit app.jsx

---
+++ src/components/Results.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Result = () => {
+    console.log("renderización de result")
+    return (
+        
+        <div className="result">
+            <span>0</span>
+        </div>
+    )
+}
+
+export default Result
