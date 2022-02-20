fdbf8271099fef49aaaf8d937d45d74823767842
Ericka Diaz <erickaldiaz@gmail.com>
2022-02-20 01:57:15 -0300

    simplification in Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({type, text, clickHandler}) => {
-    return (
-        <button className = {type} onClick={() => { 
+//componente funcional
+
+const Button = ({type, text, clickHandler}) => (
+        <button className = {type} onClick={() =>
             // console.log("onClick en el Button")
-        clickHandler(text)
-        }}>
+            clickHandler(text)
+        }>
             <span>{text}</span>
         </button>
-    )
-}
+)
 
 Button.propTypes = {
     type: PropTypes.string,
