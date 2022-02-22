756ec0088dba633656323710e97986f9014a7f0b
Ericka Diaz <erickaldiaz@gmail.com>
2022-02-22 01:32:17 -0300

    create Numbers component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Button from './components/Button'
 import Functions from './components/Functions'
+import Numbers from './components/Numbers'
 import MathOperations from './components/MathOperations'
 import Result from './components/Results'
 import './App.css'
@@ -9,34 +9,22 @@ import './App.css'
 // Arrow Function
 const App = () => {
 
-  const clickHandlerFunction = text => {
-      console.log("Button.clickHandler1", text)
-  }
-
-
     // Lo que ejecuta la función
     console.log("Renderización de App")
     return (
     <main className='react-calculator'>
         <Result value={"0"}></Result>
-        <div className="numbers">
-          <Button text="1" clickHandler={clickHandlerFunction}></Button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-        </div>
-      <Functions 
+        <Numbers onClickNumber={number => {
+          console.log("Click en numbers", number)
+        }}>
+
+        </Numbers>
+        <Functions 
           onContentClear={() =>
             console.log("Content Clear")} 
           onDelete={()=>
             console.log("onDelete")}>
-      </Functions>
+        </Functions>
         <MathOperations onClickOperation={operation => 
           console.log("operation:", operation)}
           onClickEqual={equal =>
