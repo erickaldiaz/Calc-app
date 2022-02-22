1d372b8bf52d220df1eb91fa1348e7b0be4eaa60
Ericka Diaz <erickaldiaz@gmail.com>
2022-02-22 04:14:39 -0300

    add function map fot Numbers component

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
+//DRY: dont repeat yourself
 
-const Numbers = ({onClickNumber}) => (
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const renderButtons = onClickNumber => {
+    //var number = 0
+    //iterar desde el botón 1 al 0(while, for, foreach)
+    // <Button text={number.toString()} clickHandler={onClickNumber}></Button>
+ const renderButton = number => (
+<Button text={number.toString()} clickHandler={onClickNumber}></Button>
+)
+return numbers.map(renderButton)  
+
+
+}
+
+const Numbers = ({onClickNumber})=> (
     <section className="numbers">
-        <Button text="1" clickHandler={onClickNumber}></Button>
-        <Button text="2" clickHandler={onClickNumber}></Button>
-        <Button text="3" clickHandler={onClickNumber}></Button>
-        <Button text="4" clickHandler={onClickNumber}></Button>
-        <Button text="5" clickHandler={onClickNumber}></Button>
-        <Button text="6" clickHandler={onClickNumber}></Button>
-        <Button text="7" clickHandler={onClickNumber}></Button>
-        <Button text="8" clickHandler={onClickNumber}></Button>
-        <Button text="9" clickHandler={onClickNumber}></Button>
-        <Button text="0" clickHandler={onClickNumber}></Button>
+        {
+            renderButtons(onClickNumber)
+        }
     </section>
 )
+
 
 Numbers.propTypes = {
     onClickNumber: PropTypes.func.isRequired,
